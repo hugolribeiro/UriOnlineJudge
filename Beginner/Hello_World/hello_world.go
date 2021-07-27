7c9ba539f5a8bda6296fe04b31de3ca04ca7561d
Hugo <hugolerib@gmail.com>
2021-07-27 10:19:23 -0300

    Hello_World

---
+++ Beginner/Hello_World/hello_world.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello World!")
+
+	/**
+	 * Escreva a sua solução aqui
+	 * Code your solution here
+	 * Escriba su solución aquí
+	 */
+
+}
