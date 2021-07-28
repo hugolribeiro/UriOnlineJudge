bf2d98cd3b084cebaee2f31f7bece0f119d426f9
Hugo <hugolerib@gmail.com>
2021-07-28 14:10:08 -0300

    Simple Product exercise

---
+++ Beginner/Simple_Product/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	/**
+	 * Escreva a sua solução aqui
+	 * Code your solution here
+	 * Escriba su solución aquí
+	 */
+	var a int
+	fmt.Scan(&a)
+	var b int
+	fmt.Scan(&b)
+	prod := a * b
+	fmt.Printf("PROD = %d\n", prod)
+}
